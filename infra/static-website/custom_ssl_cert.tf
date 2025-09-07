4576cd607fcf104f3af5b248a688d2618a781407
Marouane Bouaricha <marouane.tchiko@gmail.com>
2025-09-07 22:58:47 +0100

    Remove duplicate resource

--- infra/static-website/custom_ssl_cert.tf
+++ infra/static-website/custom_ssl_cert.tf
@@ -29,15 +29,3 @@ resource "aws_route53_record" "ssl_cert_validation_records" {
   zone_id         = var.route53_zone_id
 }
 
-
-resource "aws_route53_record" "static_website_A_Record" {
-  zone_id = var.route53_zone_id
-  name    = var.domain
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.static_site_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.static_site_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
