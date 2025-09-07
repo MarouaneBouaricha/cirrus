5c2bebd3c0b0f6e8303b40f9eeefeeb824bd64c6
Marouane Bouaricha <marouane.tchiko@gmail.com>
2025-09-07 22:54:59 +0100

    Added custom ssl request

---
+++ infra/static-website/custom_ssl_cert.tf
@@ -0,0 +1,43 @@
+resource "aws_acm_certificate" "ssl_cert" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+
+  tags = {
+    Name        = "static-website-page"
+    Environment = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "ssl_cert_validation_records" {
+  for_each = {
+    for dvo in aws_acm_certificate.ssl_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.route53_zone_id
+}
+
+
+resource "aws_route53_record" "static_website_A_Record" {
+  zone_id = var.route53_zone_id
+  name    = var.domain
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.static_site_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.static_site_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
