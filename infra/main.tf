32664fa25924d159390806b67525291b8305ee33
Marouane Bouaricha <marouane.tchiko@gmail.com>
2025-09-07 23:03:26 +0100

    Added module variables

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,8 @@
 module "static-website" {
-  source = "../static-website"
+  source = "./static-website"
+
+  environment = var.environment
+  route53_zone_id = var.route53_zone_id
+  domain = var.domain
+  region = var.region
 }
